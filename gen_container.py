d440a81958bb87baa28e2b88e359abcad79d1f97
Kaushik Chandrasekhar <kaushikc92@gmail.com>
2020-03-04 11:21:47 +0000

    Updated gen_container.py to work on windows

--- gen_container.py
+++ gen_container.py
@@ -1,34 +1,50 @@
-import argparse, os, subprocess
+import argparse, os, subprocess, shutil
 
-folder_name = "mapfn-foo"
+def copy_dir(src, dst):
+    dst = os.path.join(dst, os.path.dirname(src))
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d)
+        else:
+            shutil.copy2(s, d) 
 
-parser = argparse.ArgumentParser(description="Generate Dockerfile for container")
-parser.add_argument("-n", "--name", help="Folder name")
-parser.add_argument("-f", "--profiler", help="Profiling function")
-parser.add_argument("-r", "--requirements", help="Pip requirements file")
-parser.add_argument("-m", "--modules", help="Additional python modules folder")
-parser.add_argument("-l", "--local", help="Path to local pip packages")
-options = parser.parse_args()
+def main():
+    folder_name = "mapfn-foo"
 
-base_path = os.path.join(os.path.join(options.name, "src"), "foo")
+    parser = argparse.ArgumentParser(description="Generate Dockerfile for container")
+    parser.add_argument("-n", "--name", help="Folder name")
+    parser.add_argument("-f", "--profiler", help="Profiling function")
+    parser.add_argument("-r", "--requirements", help="Pip requirements file")
+    parser.add_argument("-m", "--modules", help="Additional python modules folder")
+    parser.add_argument("-l", "--local", help="Path to local pip packages")
+    options = parser.parse_args()
 
-project_name = None
-if options.name is not None:
-    project_name = options.name
+    if options.name is not None:
+        folder_name = options.name
+    base_path = os.path.join(os.path.join(folder_name, "src"), "foo")
 
-subprocess.call(["git", "clone", "https://www.github.com/columbustech/map-container-template"])
-subprocess.call(["mv", "map-container-template", options.name])
-subprocess.call(["cp", options.profiler, os.path.join(base_path, "process.py")])
+    subprocess.call(["git", "clone", "https://www.github.com/columbustech/map-container-template"])
+    shutil.move("map-container-template", folder_name)
 
-if options.modules is not None:
-    subprocess.call(["cp", "-r", options.modules, base_path])
+    if options.profiler is None:
+        print("Please provide a process function with the -f flag")
 
-if options.requirements is not None:
-    req = None
-    with open(options.requirements, "r") as f:
-        req = f.read()
-    with open(os.path.join(options.name, "requirements.txt"), "a") as f:
-        f.write(req)
+    shutil.copy(options.profiler, os.path.join(base_path, "process.py"))
 
-if options.local is not None:
-    subprocess.call(["cp", "-r", options.local, options.name])
+    if options.modules is not None:
+        shutil.copytree(options.modules, os.path.join(base_path, os.path.dirname(options.modules)))
+
+    if options.requirements is not None:
+        req = None
+        with open(options.requirements, "r") as f:
+            req = f.read()
+        with open(os.path.join(folder_name, "requirements.txt"), "a") as f:
+            f.write(req)
+
+    if options.local is not None:
+        shutil.copytree(options.local, os.path.join(folder_name, os.path.dirname(options.local)))
+
+if __name__ == "__main__":
+    main()
